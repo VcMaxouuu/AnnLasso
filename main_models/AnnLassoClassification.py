97ec3e145ef69a6c5577474d40d01633768c6663
maxime van cutsem <maxime.van-cutsem@etu.unige.ch>
2024-03-18 11:21:04 +0100

    m

---
+++ main_models/AnnLassoClassification.py
@@ -0,0 +1,11 @@
+import torch
+from torch.nn import Linear
+import numpy as np
+import utils
+import time
+from tqdm import tqdm
+import sub_models as models
+
+
+class AnnLassoClassification(GeneralModel):
+    pass
