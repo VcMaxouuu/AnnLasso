fe159bc631129586376ede8a3e980ad1484c59e0
MaximeVanCutsem <maxime.vancutsem@unige.ch>
2024-12-09 10:26:23 +0100

    commit additional folders

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="harderLASSO",  # Replace with your package name
+    version="0.1.0",
+    description="Feature selection and data reduction through neural network training.",
+    author="Maxime Van Cutsem",
+    author_email="maxime.vancutsem@unige.ch",
+    packages=find_packages(),  # Automatically finds sub-packages
+    install_requires=[],  # Add required packages here, e.g., ['numpy']
+)
